dc90d15fb2e1d93bb9978bb17e12dcb5928bb1da
xltan98 <xinnli98@gmail.com>
2023-06-20 12:24:49 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/paf21_jul2023/Paf21Jul2023Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf21_jul2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf21Jul2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf21Jul2023Application.class, args);
+	}
+
+}
